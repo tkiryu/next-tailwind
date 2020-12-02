c0d30c51f72dde0819df61e4f3a0eb1e6750e7aa
Tatsushi Kiryu <mr.mistyrain@gmail.com>
2020-12-02 22:45:59 +0900

    configure tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./pages/**/*.{js|jsx}'], // add any files that reference any tailwindcss classes.
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
