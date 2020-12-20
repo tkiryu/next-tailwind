e2e6f5cfaca860f123b91dfa482955396deb9456
Tatsushi Kiryu <tatsushi_kiryu@caddi.jp>
2020-12-20 09:54:31 +0900

    fix purge config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./pages/**/*.{js|jsx}'], // add any files that reference any tailwindcss classes.
+  purge: ['./pages/**/*.{js,jsx}'], // add any files that reference any tailwindcss classes.
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
